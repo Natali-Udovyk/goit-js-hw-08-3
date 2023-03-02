601558096f2a3d4645377769051d3bb267e0b501
Natalina <natali.udovik@gmail.com>
2023-03-02 19:38:00 +0200

    vimeo-player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+/* <iframe src="https://player.vimeo.com/video/76979871?h=8272103f6e" width="640" height="360" frameborder="0" allowfullscreen allow="autoplay; encrypted-media"></iframe> */
+
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const currentTime = localStorage.getItem("videoplayer-current-time");
+if (currentTime > 0) {
+    player.setCurrentTime(currentTime);
+}
+
+function updateCurrentTime(event) {
+    localStorage.setItem("videoplayer-current-time", event.seconds);
+}
+
+player.on('timeupdate', throttle(updateCurrentTime, 1000));
 
