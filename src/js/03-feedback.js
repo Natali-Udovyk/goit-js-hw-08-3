616ccb8ddef3f1f316fdad8272f636ec35e9c82b
Natalina <natali.udovik@gmail.com>
2023-03-02 22:59:46 +0200

    form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,19 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector(".feedback-form");
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+form.addEventListener("input", throttle((event) => {
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify({
+        email: form.email.value,
+        message: form.message.value
+    }));
+}, 500));
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(localStorage.getItem(LOCALSTORAGE_KEY));
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    form.reset();
+});
 
